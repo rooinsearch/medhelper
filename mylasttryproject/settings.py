4263de51cae436d38c299126481a085cf621922d
Arussol <ardaqaruzhan04@gmail.com>
2025-05-13 11:55:36 +0500

    Fix ALLOWED_HOSTS parsing for Render

--- mylasttryproject/settings.py
+++ mylasttryproject/settings.py
@@ -21,7 +21,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 
 SECRET_KEY=env('SECRET_KEY')
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", default="").split(",")
 
 
 INSTALLED_APPS = [
