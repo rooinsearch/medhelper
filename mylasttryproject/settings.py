3e9d45edd384c447c89b2ec58f69e22f64209625
Arussol <ardaqaruzhan04@gmail.com>
2025-05-13 11:26:23 +0500

    Fix STATIC_URL path for staticfiles

--- mylasttryproject/settings.py
+++ mylasttryproject/settings.py
@@ -165,8 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # для Render
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
